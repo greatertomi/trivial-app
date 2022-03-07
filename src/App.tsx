de6d2b78a6db25a284ff5d5e032ae0ac5a365f7e
John Oshalusi <john.oshalusi@spicefactory.co>
2022-03-07 14:02:24 +0100

    Add packages and initial setups

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
+import { Typography } from '@mui/material';
+import GlobalStyle from './globalStyle';
 
-function App() {
+const App = () => {
   return (
     <div>
+      <GlobalStyle />
+      <Typography>Hello now</Typography>
       Hello from here
     </div>
   );
-}
+};
 
 export default App;
