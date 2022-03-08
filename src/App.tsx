595329b49b54bd3531b26340535a684c9049be19
John Oshalusi <john.oshalusi@spicefactory.co>
2022-03-08 21:57:29 +0100

    Fetch data from api

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from '@mui/material';
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import styled from 'styled-components';
 import './App.css';
 import Navbar from './components/Navbar';
@@ -10,17 +11,19 @@ import QuizPage from './pages/QuizPage';
 import ReviewPage from './pages/ReviewPage';
 import { theme } from './theme';
 
-const PageContainer = styled.div`
+const BrowserRouter = styled(Router)`
   background-color: #f3f5f9;
   padding-bottom: 50px;
   min-height: 100vh;
 `;
 
+const queryClient = new QueryClient();
+
 const App = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <PageContainer>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
         <BrowserRouter>
           <Navbar />
           <Routes>
@@ -29,8 +32,8 @@ const App = () => {
             <Route path="/review" element={<ReviewPage />} />
           </Routes>
         </BrowserRouter>
-      </PageContainer>
-    </ThemeProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 };
 
--- src/pages/QuizPage.tsx
+++ src/pages/QuizPage.tsx
@@ -1,9 +1,11 @@
-import { Box, Button } from '@mui/material';
-import React from 'react';
+import { Alert, Box, Button, CircularProgress } from '@mui/material';
+import React, { useMemo } from 'react';
 import { BsArrowRightCircle, BsUpload } from 'react-icons/bs';
+import { useQuery } from 'react-query';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import Question from '../components/quiz/Question';
+import { QuizQuestion } from '../types/quiz';
 
 export const QuizPageContainer = styled.div`
   width: 600px;
@@ -15,31 +17,64 @@ export const QuizPageContainer = styled.div`
 `;
 
 const QuizPage = () => {
-  const hasQuestions = false;
+  const hasQuestions = true;
+  const { isLoading, error, data } =
+    useQuery('queryData', () =>
+      fetch(
+        'https://opentdb.com/api.php?amount=10&difficulty=hard&type=boolean'
+      ).then((res) => res.json())
+    ) || {};
+
+  const quizQuestions = useMemo<QuizQuestion[]>(() => {
+    return data?.results;
+  }, [data?.results]);
+
+  console.log({ quizQuestions });
+
+  const renderQuestions = () => {
+    return (
+      <>
+        {error && (
+          <Box mb={2}>
+            <Alert severity="error" title="This is cool">
+              An error occurred while fetching data
+            </Alert>
+          </Box>
+        )}
+        <Question
+          questionNumber="1 of 4"
+          category="Entertainment"
+          question="Is this good or bad?"
+        />
+        {hasQuestions ? (
+          <Button variant="contained" color="primary" size="large">
+            <Box mr={2}>Continue</Box>
+            <BsArrowRightCircle color="white" size={20} />
+          </Button>
+        ) : (
+          <Button
+            component={Link}
+            to="/review"
+            variant="contained"
+            color="primary"
+            size="large"
+          >
+            <Box mr={2}>Submit</Box>
+            <BsUpload color="white" size={20} />
+          </Button>
+        )}
+      </>
+    );
+  };
 
   return (
     <QuizPageContainer>
-      <Question
-        questionNumber="1 of 4"
-        category="Entertainment"
-        question="Is this good or bad?"
-      />
-      {hasQuestions ? (
-        <Button variant="contained" color="primary" size="large">
-          <Box mr={2}>Continue</Box>
-          <BsArrowRightCircle color="white" size={20} />
-        </Button>
+      {isLoading ? (
+        <Box>
+          <CircularProgress />
+        </Box>
       ) : (
-        <Button
-          component={Link}
-          to="/review"
-          variant="contained"
-          color="primary"
-          size="large"
-        >
-          <Box mr={2}>Submit</Box>
-          <BsUpload color="white" size={20} />
-        </Button>
+        renderQuestions()
       )}
     </QuizPageContainer>
   );
