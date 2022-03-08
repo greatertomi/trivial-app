234f75c27810f85239b6756ab85c24e1425ecf36
John Oshalusi <john.oshalusi@spicefactory.co>
2022-03-08 14:32:55 +0100

    Complete landing page

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,24 +1,27 @@
 import { Button, Typography } from '@mui/material';
 import React from 'react';
 import styled from 'styled-components';
+import { ReactComponent as QuizIcon } from '../assets/quiz.svg';
 
 const LandingPageContainer = styled.div`
   height: 80vh;
-  width: 800px;
+  width: 100%;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
+  margin-top: 50px;
 `;
 
 const LandingPage = () => {
   return (
     <LandingPageContainer>
+      <QuizIcon height={300} width={200} />
       <Typography variant="h4">Welcome to the Trivia Challenge</Typography>
-      <Typography>
+      <Typography variant="h6">
         You will be presented with 10 True or False questions
       </Typography>
-      <Typography>Can you score 100%?</Typography>
+      <Typography variant="h6">Can you score 100%?</Typography>
       <Button variant="contained" size="large" color="primary">
         Begin
       </Button>
